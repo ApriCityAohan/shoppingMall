caee65ad694aae52d6a774962cf486a7891f6e51
ApriCityAohan <1448366829@qq.com>
2023-03-11 17:59:21 +0800

    feat:图库页面基本布局实现

--- src/pages/image/List.vue
+++ src/pages/image/List.vue
@@ -1,7 +1,55 @@
 <template>
-    <div>图库列表</div>
+    <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
+        <el-header class="image-header">Header</el-header>
+        <el-container>
+            <el-aside width="220px" class="image-aside">
+                <div class="top">
+                    <div v-for="i in 100" :key="i">i</div>
+                </div>
+                <div class="bottom">分页</div>
+            </el-aside>
+            <el-main class="image-main"
+                ><div class="top">
+                    <div v-for="i in 100" :key="i">i</div>
+                </div>
+                <div class="bottom">分页</div>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
-<script setup></script>
+<script setup>
+const windowHeight = window.innerHeight || document.body.clientHeight
+const h = windowHeight - 64 - 44 - 40
+</script>
 
-<style scoped></style>
+<style scoped>
+.image-header {
+    border-bottom: 1px solid #eeeeee;
+    @apply flex items-center;
+}
+.image-aside {
+    border-right: 1px solid #eeeeee;
+    position: relative;
+}
+.image-aside .top,
+.image-main .top {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 40px;
+    overflow: auto;
+}
+.image-aside .bottom,
+.image-main .bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 40px;
+}
+.image-main {
+    position: relative;
+}
+</style>
