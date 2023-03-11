68cc99ae41775347901f1d11ce952b0baf7aa441
ApriCityAohan <1448366829@qq.com>
2023-03-11 18:00:00 +0800

    feat:全局滚动条样式优化

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,19 @@ body {
     background-color: #f4f4f4 !important;
     height: 2px !important;
 }
+::-webkit-scrollbar {
+    width: 4px;
+    height: 8px;
+}
+::-webkit-scrollbar-corner {
+    display: block;
+}
+::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+}
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0.1);
+}
 </style>
