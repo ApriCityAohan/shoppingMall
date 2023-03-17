6dd97376a07794d84c7ae285479112e45eb851bf
ApriCityAohan <1448366829@qq.com>
2023-03-17 20:14:01 +0800

    update：注释Log

--- src/router/index.js
+++ src/router/index.js
@@ -92,6 +92,6 @@ export function addRoutes(menus) {
         })
     }
     findAddRoute(menus)
-    console.log(router.getRoutes())
+    // console.log(router.getRoutes())
     return hasNewRoute
 }
