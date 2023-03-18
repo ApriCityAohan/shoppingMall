1e327db45bbbba39981431ac6cbbd5a43835e79f
ApriCityAohan <1448366829@qq.com>
2023-03-18 21:09:32 +0800

    fix：防止触发图片分类切换

--- src/components/ImgAsideList.vue
+++ src/components/ImgAsideList.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="aside-list" :class="{ active: active }">
         <span class="truncate"><slot></slot></span>
-        <el-button class="ml-auto px-1" text type="primary" size="small" @click="$emit('edit')">
+        <el-button
+            class="ml-auto px-1"
+            text
+            type="primary"
+            size="small"
+            @click.stop="$emit('edit')"
+        >
             <el-icon :size="16"><Edit /></el-icon>
         </el-button>
         <el-popconfirm
             title="是否确认删除?"
             confirm-button-text="确认"
             cancel-button-text="取消"
-            @confirm="$emit('delete')"
+            @confirm.stop="$emit('delete')"
         >
             <template #reference>
                 <el-button class="px-1" text type="primary" size="small">
