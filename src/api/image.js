6fa9a1ddb1e133213ef21f3cd675f4ee2a659bb9
ApriCityAohan <1448366829@qq.com>
2023-03-18 20:25:37 +0800

    fix：API接口参数

--- src/api/image.js
+++ src/api/image.js
@@ -2,8 +2,8 @@ import axios from '~/axios'
 export function getImageList(id, page = 1) {
     return axios.get(`/admin/image_class/${id}/image/${page}`)
 }
-export function updateImage(id, data) {
-    return axios.post(`/admin/image/${id}`, data)
+export function updateImage(id, name) {
+    return axios.post(`/admin/image/${id}`, { name })
 }
 export function deleteImage(ids) {
     return axios.get(`/admin/image/delete_all`, { ids })
