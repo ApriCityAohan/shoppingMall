53fd5059063e19fd310068c05f0398f7af7e229b
ApriCityAohan <1448366829@qq.com>
2023-03-20 15:12:07 +0800

    update:更新公告样式代码

--- src/pages/notice/List.vue
+++ src/pages/notice/List.vue
@@ -1,7 +1,62 @@
 <template>
-    <div>公告列表</div>
+    <el-card shadow="never">
+        <div class="flex justify-between items-center mb-3">
+            <el-button type="primary" size="small">新增</el-button>
+            <el-button text size="small">
+                <el-icon :size="20"><Refresh /></el-icon>
+            </el-button>
+        </div>
+        <el-table :data="tableData" stripe style="width: 100%">
+            <el-table-column prop="title" label="公告标题" />
+            <el-table-column prop="create_time" label="发布时间" width="320" />
+            <el-table-column label="操作" width="180" align="center">
+                <template #default="scope">
+                    <el-button
+                        text
+                        type="primary"
+                        size="small"
+                        @click="handleNoticeEdit(scope.row)"
+                    >
+                        修改
+                    </el-button>
+                    <el-popconfirm
+                        title="是否要删除公告?"
+                        confirm-button-text="确认"
+                        cancel-button-text="取消"
+                        @confirm="handleNoticeDelete(scope.row)"
+                    >
+                        <template #reference>
+                            <el-button text type="primary" size="small">删除</el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-card>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+const tableData = reactive([
+    {
+        title: 'Tom',
+        create_time: '2016-05-03'
+    },
+    {
+        title: 'Tom',
+        create_time: '2016-05-03'
+    },
+    {
+        title: 'Tom',
+        create_time: '2016-05-03'
+    }
+])
+const handleNoticeEdit = row => {
+    console.log(row)
+}
+const handleNoticeDelete = row => {
+    console.log(row)
+}
+</script>
 
 <style scoped></style>
