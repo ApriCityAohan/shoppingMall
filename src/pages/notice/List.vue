bef62b89f080a9be51e95d005ba70d4ca57649aa
ApriCityAohan <1448366829@qq.com>
2023-03-20 19:25:53 +0800

    feat：更新了列表交互

--- src/pages/notice/List.vue
+++ src/pages/notice/List.vue
@@ -2,11 +2,11 @@
     <el-card shadow="never">
         <div class="flex justify-between items-center mb-3">
             <el-button type="primary" size="small">新增</el-button>
-            <el-button text size="small">
+            <el-button text size="small" @click="getData">
                 <el-icon :size="20"><Refresh /></el-icon>
             </el-button>
         </div>
-        <el-table :data="tableData" stripe style="width: 100%">
+        <el-table v-loading="loading" :data="tableData" stripe style="width: 100%">
             <el-table-column prop="title" label="公告标题" />
             <el-table-column prop="create_time" label="发布时间" width="320" />
             <el-table-column label="操作" width="180" align="center">
@@ -32,28 +32,57 @@
                 </template>
             </el-table-column>
         </el-table>
+        <div class="flex justify-center items-center mt-5">
+            <el-pagination
+                v-model:current-page="currentPage"
+                background
+                layout="prev, pager, next"
+                :total="total"
+                :page-size="limit"
+                @current-change="getData"
+            />
+        </div>
     </el-card>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-const tableData = reactive([
-    {
-        title: 'Tom',
-        create_time: '2016-05-03'
-    },
-    {
-        title: 'Tom',
-        create_time: '2016-05-03'
-    },
+import { ref } from 'vue'
+import { getNoticeList } from '~/api/notice.js'
+// 公告列表
+const tableData = ref([
     {
         title: 'Tom',
         create_time: '2016-05-03'
     }
 ])
+// Loading状态
+const loading = ref(false)
+// 页码
+const currentPage = ref(1)
+const total = ref(0)
+const limit = ref(10)
+// 获取公告列表
+function getData(page) {
+    if (typeof page === 'number') {
+        currentPage.value = page
+    }
+    loading.value = true
+    getNoticeList(currentPage.value)
+        .then(res => {
+            console.log(res)
+            tableData.value = res.list
+            total.value = res.totalCount
+        })
+        .finally(() => {
+            loading.value = false
+        })
+}
+getData()
+// 修改公告
 const handleNoticeEdit = row => {
     console.log(row)
 }
+// 删除公告
 const handleNoticeDelete = row => {
     console.log(row)
 }
