9a7733dd74f01911ab7df0a7dc7c20911b329c33
ApriCityAohan <1448366829@qq.com>
2023-03-21 21:07:35 +0800

    update:添加新增、修改、删除管理员API

--- src/api/manager.js
+++ src/api/manager.js
@@ -6,15 +6,19 @@ export function login(username, password) {
         password
     })
 }
+
 export function getInfo() {
     return axios.post('/admin/getinfo')
 }
+
 export function logout() {
     return axios.post('/admin/logout')
 }
+
 export function updatepassword(data) {
     return axios.post('/admin/updatepassword', data)
 }
+
 export function getManagerList(page, query = {}) {
     const q = []
     for (const key in query) {
@@ -26,6 +30,19 @@ export function getManagerList(page, query = {}) {
     r = r ? '?' + r : ''
     return axios.get(`/admin/manager/${page}${r}`)
 }
+
 export function updateManagerStatus(id, status) {
     return axios.post(`/admin/manager/${id}/update_status`, { status })
+}
+
+export function createManager(data) {
+    return axios.post(`/admin/manager`, data)
+}
+
+export function updateManager(id, data) {
+    return axios.post(`/admin/manager/${id}`, data)
+}
+
+export function deleteManager(id) {
+    return axios.post(`/admin/manager/${id}/delete`)
 }
