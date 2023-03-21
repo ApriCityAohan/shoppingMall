efbeb86b68adec0045319d8982e828c30f95c1f4
ApriCityAohan <1448366829@qq.com>
2023-03-21 15:41:34 +0800

    update：新增获取管理员API

--- src/api/manager.js
+++ src/api/manager.js
@@ -15,3 +15,14 @@ export function logout() {
 export function updatepassword(data) {
     return axios.post('/admin/updatepassword', data)
 }
+export function getManagerList(page, query = {}) {
+    const q = []
+    for (const key in query) {
+        if (query[key]) {
+            q.push(`${key}=${encodeURIComponent(query[key])}`)
+        }
+    }
+    let r = q.join('&')
+    r = r ? '?' + r : ''
+    return axios.get(`/admin/manager/${page}${r}`)
+}
