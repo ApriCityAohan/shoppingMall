ab89eb1d34e727d17d9e1991f04a8fc23b99d918
ApriCityAohan <1448366829@qq.com>
2023-03-24 22:03:52 +0800

    feat:新增树形列表获取

--- src/pages/access/List.vue
+++ src/pages/access/List.vue
@@ -1,11 +1,32 @@
 <template>
     <el-card shadow="never">
-        <ListHeader />
+        <ListHeader @refresh="getData" />
+        <el-tree
+            v-loading="loading"
+            :data="tableData"
+            :props="{ label: 'name', children: 'child' }"
+            node-key="id"
+            :default-expanded-keys="defaultExpandedKeys"
+        />
     </el-card>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ListHeader from '~/components/ListHeader.vue'
+// eslint-disable-next-line no-unused-vars
+import { getRuleList } from '~/api/rule'
+import { initTableData } from '~/utils/useCommon'
+
+const defaultExpandedKeys = ref([])
+
+const { loading, tableData, getData } = initTableData({
+    getListFun: getRuleList,
+    onGetListSuccess: res => {
+        tableData.value = res.list
+        defaultExpandedKeys.value = res.list.map(o => o.id)
+    }
+})
 </script>
 
 <style scoped></style>
