9046811d4ef124bef1a9dc77e5d3a41769e64f0d
ApriCityAohan <1448366829@qq.com>
2023-03-25 14:39:45 +0800

    style:优化树形控件的样式

--- src/pages/access/List.vue
+++ src/pages/access/List.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-unused-vars -->
 <template>
     <el-card shadow="never">
         <ListHeader @refresh="getData" />
@@ -7,7 +8,26 @@
             :props="{ label: 'name', children: 'child' }"
             node-key="id"
             :default-expanded-keys="defaultExpandedKeys"
-        />
+        >
+            <template #default="{ node, data }">
+                <div class="custom-tree-node">
+                    <el-tag :type="data.menu ? '' : 'info'" size="small">{{
+                        data.menu ? '菜单' : '权限'
+                    }}</el-tag>
+                    <el-icon v-if="data.icon" size="16" class="ml-2">
+                        <component :is="data.icon"></component>
+                    </el-icon>
+                    <span class="ml-2">{{ data.name }}</span>
+                    <div class="ml-auto">
+                        <el-switch :model-value="data.status" :active-value="1" :inactive-value="0">
+                        </el-switch>
+                        <el-button text type="primary" size="small">修改</el-button>
+                        <el-button text type="primary" size="small">添加</el-button>
+                        <el-button text type="primary" size="small">删除</el-button>
+                    </div>
+                </div>
+            </template>
+        </el-tree>
     </el-card>
 </template>
 
@@ -29,4 +49,15 @@ const { loading, tableData, getData } = initTableData({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    padding-right: 8px;
+}
+:deep(.el-tree-node__content) {
+    @apply py-5;
+}
+</style>
