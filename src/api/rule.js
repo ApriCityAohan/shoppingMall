e59e0ae4c813da9fef034f92ddd550616e00e256
ApriCityAohan <1448366829@qq.com>
2023-03-25 16:45:01 +0800

    update:新增菜单添加和菜单修改API

--- src/api/rule.js
+++ src/api/rule.js
@@ -2,3 +2,9 @@ import axios from '~/axios'
 export function getRuleList(page) {
     return axios.get(`/admin/rule/${page}`)
 }
+export function createRule(data) {
+    return axios.post(`/admin/rule`, data)
+}
+export function updateRule(id, data) {
+    return axios.post(`/admin/rule/${id}`, data)
+}
