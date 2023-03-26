9f3d24f9e165da99161fe308de2d4515eabb5385
ApriCityAohan <1448366829@qq.com>
2023-03-26 19:01:46 +0800

    feat(api): 添加了{Role}的API实现

---
+++ src/api/role.js
@@ -0,0 +1,16 @@
+import axios from '~/axios'
+export function getRoleList(page) {
+    return axios.get(`/admin/role/${page}`)
+}
+export function createRole(data) {
+    return axios.post(`/admin/role`, data)
+}
+export function updateRole(id, data) {
+    return axios.post(`/admin/role/${id}`, data)
+}
+export function deleteRole(id) {
+    return axios.post(`/admin/role/${id}/delete`)
+}
+export function updateRoleStatus(id, status) {
+    return axios.post(`/admin/role/${id}/update_status`, { status })
+}
