2397d9970aa94fc96fe3aac5ee9b8870ecd877cb
ApriCityAohan <1448366829@qq.com>
2023-03-28 17:51:20 +0800

    feat(Coupon):编写了基础渲染和交互

--- src/pages/coupon/List.vue
+++ src/pages/coupon/List.vue
@@ -1,7 +1,115 @@
 <template>
-    <div>优惠券列表</div>
+    <el-card shadow="never">
+        <ListHeader @create="handleAdd" @refresh="getData" />
+        <el-table v-loading="loading" :data="tableData" stripe style="width: 100%">
+            <el-table-column label="优惠券名称" width="350">
+                <template #default="{ row }">
+                    <div class="border border-dashed rounded py-2 px-4">
+                        <h5 class="font-bold text-sm">{{ row.name }}</h5>
+                        <small>{{ row.start_time }} ~ {{ row.end_time }}</small>
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="status" label="状态" align="center" />
+            <el-table-column label="优惠" align="center">
+                <template #default="{ row }">
+                    <span>
+                        {{ row.type ? '折扣' : '满减' }}
+                        {{ row.type ? row.value + '折' : '￥' + row.value }}
+                    </span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="total" label="发放数量" align="center" />
+            <el-table-column prop="used" label="已使用" align="center" />
+            <el-table-column label="操作" width="180" align="center">
+                <template #default="scope">
+                    <el-button text type="primary" size="small" @click="handleEdit(scope.row)">
+                        修改
+                    </el-button>
+                    <el-popconfirm
+                        title="是否要删除公告?"
+                        confirm-button-text="确认"
+                        cancel-button-text="取消"
+                        @confirm="handleDelete(scope.row.id)"
+                    >
+                        <template #reference>
+                            <el-button text type="primary" size="small">删除</el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div class="flex justify-center items-center mt-5">
+            <el-pagination
+                v-model:current-page="currentPage"
+                background
+                layout="prev, pager, next"
+                :total="total"
+                :page-size="limit"
+                @current-change="getData"
+            />
+        </div>
+        <Drawer ref="drawerRef" :title="drawerTitle" @submit="handleSubmit">
+            <el-form ref="formRef" :model="form" :rules="rules" label-width="80px" :inline="false">
+                <el-form-item label="公告标题" prop="title">
+                    <el-input v-model="form.title" placeholder="公告标题"></el-input>
+                </el-form-item>
+                <el-form-item label="公告内容" prop="content">
+                    <el-input
+                        v-model="form.content"
+                        :rows="5"
+                        type="textarea"
+                        placeholder="公告内容"
+                    />
+                </el-form-item>
+            </el-form>
+        </Drawer>
+    </el-card>
 </template>
 
-<script setup></script>
+<script setup>
+import {
+    getCouponList,
+    createCoupon,
+    updateCoupon,
+    deleteCoupon,
+    updateCouponStatus
+} from '~/api/coupon.js'
+import Drawer from '~/components/Drawer.vue'
+import ListHeader from '~/components/ListHeader.vue'
+import { initTableData, initForm } from '~/utils/useCommon.js'
+const { tableData, loading, currentPage, total, limit, getData, handleDelete } = initTableData({
+    getListFun: getCouponList,
+    delete: deleteCoupon,
+    updateStatus: updateCouponStatus
+})
+const { drawerRef, formRef, form, rules, drawerTitle, handleAdd, handleEdit, handleSubmit } =
+    initForm({
+        getData,
+        form: {
+            title: '',
+            content: ''
+        },
+        rules: {
+            title: [
+                {
+                    required: true,
+                    message: '请输入公告标题',
+                    trigger: 'blur'
+                }
+            ],
+            content: [
+                {
+                    required: true,
+                    message: '请输入公告内容',
+                    trigger: 'blur'
+                }
+            ]
+        },
+        create: createCoupon,
+        update: updateCoupon,
+        loading
+    })
+</script>
 
 <style scoped></style>
