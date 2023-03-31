de3d2eb34794280d33b8703215277809d8cdaada
ApriCityAohan <1448366829@qq.com>
2023-03-31 15:53:16 +0800

    feat：新增编辑抽屉和功能实现

--- src/pages/goods/List.vue
+++ src/pages/goods/List.vue
@@ -9,7 +9,6 @@
             >
             </el-tab-pane>
         </el-tabs>
-
         <el-card shadow="never">
             <div class="mb-3">
                 <Search :model="searchForm" @search="getData" @reset="handleResetSearch">
@@ -38,50 +37,6 @@
                         </SearchItem>
                     </template>
                 </Search>
-                <!-- <el-form :model="searchForm" label-width="80px" size="small">
-                    <el-row :gutter="20">
-                        <el-col :span="8" :offset="0">
-                            <el-form-item label="关键词">
-                                <el-input
-                                    v-model="searchForm.title"
-                                    placeholder="商品名称"
-                                    clearable
-                                ></el-input>
-                            </el-form-item>
-                        </el-col>
-                        <el-col v-if="showSearch" :span="8">
-                            <el-form-item label="商品分类">
-                                <el-select
-                                    v-model="searchForm.category_id"
-                                    placeholder="请选择商品类别"
-                                    clearable
-                                >
-                                    <el-option
-                                        v-for="item in categoryList"
-                                        :key="item.id"
-                                        :label="item.name"
-                                        :value="item.id"
-                                    >
-                                    </el-option>
-                                </el-select>
-                            </el-form-item>
-                        </el-col>
-                        <el-col :span="8" :offset="showSearch ? 0 : 8">
-                            <div class="flex items-center justify-end">
-                                <el-button type="primary" @click="getData">搜索</el-button>
-                                <el-button @click="handleResetSearch">重置</el-button>
-                                <el-button text type="primary" @click="showSearch = !showSearch">
-                                    {{ showSearch ? '收起' : '展开' }}
-                                    <el-icon
-                                        class="transform transition-all ml-1"
-                                        :class="showSearch ? 'rotate-180' : ''"
-                                        ><ArrowUp
-                                    /></el-icon>
-                                </el-button>
-                            </div>
-                        </el-col>
-                    </el-row>
-                </el-form> -->
             </div>
             <ListHeader @create="handleAdd" @refresh="getData" />
             <el-table v-loading="loading" :data="tableData" stripe style="width: 100%">
@@ -194,22 +149,22 @@
                     ref="formRef"
                     :model="form"
                     :rules="rules"
-                    label-width="80px"
+                    label-width="90px"
                     :inline="false"
                 >
-                    <el-form-item label="用户名" prop="username">
-                        <el-input v-model="form.username" placeholder="用户名"></el-input>
+                    <el-form-item label="商品名称" prop="title">
+                        <el-input
+                            v-model="form.title"
+                            placeholder="请输入商品名称，不能超过60个字符"
+                        ></el-input>
                     </el-form-item>
-                    <el-form-item label="密码" prop="password">
-                        <el-input v-model="form.password" placeholder="密码"></el-input>
+                    <el-form-item label="封面">
+                        <ChooseImage v-model="form.cover" />
                     </el-form-item>
-                    <el-form-item label="头像">
-                        <ChooseImage v-model="form.avatar" />
-                    </el-form-item>
-                    <el-form-item label="所属角色" prop="role_id">
-                        <el-select v-model="form.role_id" placeholder="选择所属角色">
+                    <el-form-item label="商品分类" prop="category_id">
+                        <el-select v-model="form.category_id" placeholder="选择商品分类">
                             <el-option
-                                v-for="item in roles"
+                                v-for="item in categoryList"
                                 :key="item.id"
                                 :label="item.name"
                                 :value="item.id"
@@ -217,9 +172,43 @@
                             </el-option>
                         </el-select>
                     </el-form-item>
-                    <el-form-item label="状态" prop="status">
-                        <el-switch v-model="form.status" :active-value="1" :inactive-value="0">
-                        </el-switch>
+                    <el-form-item label="商品描述" prop="desc">
+                        <el-input v-model="form.desc" type="textarea"></el-input>
+                    </el-form-item>
+                    <el-form-item label="商品单位" prop="unit" style="width: 50%">
+                        <el-input v-model="form.unit"></el-input>
+                    </el-form-item>
+                    <el-form-item label="总库存" prop="stock" style="width: 40%">
+                        <el-input v-model="form.stock" type="number">
+                            <template #append>件</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item label="库存预警" prop="min_stock" style="width: 40%">
+                        <el-input v-model="form.min_stock" type="number">
+                            <template #append>件</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item label="最低销售价" prop="min_price" style="width: 40%">
+                        <el-input v-model="form.min_price" type="number">
+                            <template #append>元</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item label="最低原价" prop="min_oprice" style="width: 40%">
+                        <el-input v-model="form.min_oprice" type="number">
+                            <template #append>元</template>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item label="库存显示" prop="stock_display">
+                        <el-radio-group v-model="form.stock_display">
+                            <el-radio :label="1" border>是</el-radio>
+                            <el-radio :label="0" border>否</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="是否上架" prop="status">
+                        <el-radio-group v-model="form.status">
+                            <el-radio :label="0" border>放入仓库</el-radio>
+                            <el-radio :label="1" border>立即上架</el-radio>
+                        </el-radio-group>
                     </el-form-item>
                 </el-form>
             </Drawer>
@@ -269,7 +258,6 @@ const {
             return o
         })
         total.value = res.totalCount
-        roles.value = res.roles
     },
     delete: deleteGoods,
     updateStatus: updateGoodsStatus
@@ -278,18 +266,22 @@ const { drawerRef, formRef, form, rules, drawerTitle, handleAdd, handleEdit, han
     initForm({
         getData,
         form: {
-            username: '',
-            password: '',
-            avatar: '',
-            role_id: '',
-            status: 0
+            title: '', // 商品名称
+            category_id: null, // 商品分类
+            cover: null, // 商品封面
+            desc: '', // 商品描述
+            unit: '件', // 商品单位
+            stock: 100, // 总库存
+            min_stock: 10, // 库存预警
+            status: 1, // 是否上架 0仓库1上架
+            stock_display: 1, // 库存显示 0隐藏1显示
+            min_price: 0, // 最低销售价
+            min_oprice: 0 // 最低原价
         },
         create: createGoods,
         update: updateGoods,
         loading
     })
-// 下拉框数据
-const roles = ref([])
 // tabBar数据
 const tabBars = ref([
     {
