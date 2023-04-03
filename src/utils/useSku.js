ebdbea0f3be2635f1f348e6bf20dc22f5ef4b9c4
ApriCityAohan <1448366829@qq.com>
2023-04-03 17:07:28 +0800

    feat：新增添加规格功能实现

--- src/utils/useSku.js
+++ src/utils/useSku.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { createGoodSku } from '~/api/goods.js'
 // 商品id
 export const goodsId = ref(0)
 // 商品sku list
@@ -15,7 +16,28 @@ export function initSkuCardList(list) {
         return item
     })
 }
-// 选择sku
+export const btnLoading = ref(false)
+export function createGoodSkuOption() {
+    btnLoading.value = true
+    createGoodSku({
+        goods_id: goodsId.value, // 商品ID
+        name: '规格名称', // 规格名称
+        order: 50, // 排序
+        type: 0 // 规格类型 0文字
+    })
+        .then(res => {
+            skuCardList.value.push({
+                ...res,
+                text: res.name,
+                loading: false,
+                goodsSkusCardValue: []
+            })
+        })
+        .finally(() => {
+            btnLoading.value = false
+        })
+}
+// 初始化sku规格选项详情
 export function initSkuCardValue(id) {
     const item = skuCardList.value.find(o => o.id === id)
     return { item }
