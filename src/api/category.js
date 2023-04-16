400b74e24c31c66588853f0f8fd8259b51d00b29
ApriCityAohan <1448366829@qq.com>
2023-04-16 17:04:45 +0800

    feat:获取推荐列表数据api

--- src/api/category.js
+++ src/api/category.js
@@ -15,3 +15,6 @@ export function updateCategoryStatus(id, status) {
 export function deleteCategory(id) {
     return axios.post(`/admin/category/${id}/delete`)
 }
+export function getCategoryGoods(id) {
+    return axios.get(`/admin/app_category_item/list?category_id=${id}`)
+}
