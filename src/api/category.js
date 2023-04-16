3c29ea87c30073a62773a5fcb5362d5cd84d37b6
ApriCityAohan <1448366829@qq.com>
2023-04-16 15:48:56 +0800

    feat:新增分类API接口

--- src/api/category.js
+++ src/api/category.js
@@ -3,3 +3,15 @@ import axios from '~/axios'
 export function getCategoryList() {
     return axios.get(`/admin/category`)
 }
+export function createCategory(data) {
+    return axios.post(`/admin/category`, data)
+}
+export function updateCategory(id, data) {
+    return axios.post(`/admin/category/${id}`, data)
+}
+export function updateCategoryStatus(id, status) {
+    return axios.post(`/admin/category/${id}/update_status`, { status })
+}
+export function deleteCategory(id) {
+    return axios.post(`/admin/category/${id}/delete`)
+}
