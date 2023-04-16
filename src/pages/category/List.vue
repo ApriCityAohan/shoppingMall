eb5e6fc3dcb54038763634fb1c3e3a8bd459e421
ApriCityAohan <1448366829@qq.com>
2023-04-16 16:43:21 +0800

    feat：完善了修改和添加功能

--- src/pages/category/List.vue
+++ src/pages/category/List.vue
@@ -41,56 +41,9 @@
             </template>
         </el-tree>
         <Drawer ref="drawerRef" :title="drawerTitle" @submit="handleSubmit">
-            <el-form ref="formRef" :model="form" :rules="rules" label-width="80px" :inline="false">
-                <el-form-item label="上级菜单" prop="rule_id">
-                    <el-cascader
-                        v-model="form.rule_id"
-                        :options="options"
-                        :props="{
-                            value: 'id',
-                            label: 'name',
-                            children: 'child',
-                            checkStrictly: true,
-                            emitPath: false,
-                            expandTrigger: 'hover'
-                        }"
-                        placeholder="选择上级菜单"
-                    />
-                </el-form-item>
-                <el-form-item label="菜单/规则" prop="menu">
-                    <el-radio-group v-model="form.menu">
-                        <el-radio :label="1" border>菜单</el-radio>
-                        <el-radio :label="0" border>规则</el-radio>
-                    </el-radio-group>
-                </el-form-item>
-                <el-form-item label="名称" prop="name" style="width: 30%">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item v-if="form.menu === 1" label="菜单图标" prop="icon">
-                    <IconSelect v-model="form.icon" />
-                </el-form-item>
-                <el-form-item v-if="form.menu === 0" label="后端规则" prop="frontpath">
-                    <el-input v-model="form.frontpath" placeholder="后端规则"></el-input>
-                </el-form-item>
-                <el-form-item
-                    v-if="form.menu === 1 && form.rule_id > 0"
-                    label="前端路由"
-                    prop="condition"
-                >
-                    <el-input v-model="form.condition" placeholder="前端路由路径"></el-input>
-                </el-form-item>
-                <el-form-item v-if="form.menu === 0" label="请求方式" prop="method">
-                    <el-select v-model="form.method" placeholder="请选择请求方式">
-                        <el-option
-                            v-for="item in ['GET', 'POST', 'PUT', 'DELETE']"
-                            :key="item"
-                            :label="item"
-                            :value="item"
-                        />
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="排序" prop="order">
-                    <el-input-number v-model="form.order" :min="0" :max="10000" />
+            <el-form ref="formRef" :model="form" label-width="80px" :inline="false">
+                <el-form-item label="分类名称" prop="name">
+                    <el-input v-model="form.name" placeholder="分类名称" />
                 </el-form-item>
             </el-form>
         </Drawer>
@@ -101,7 +54,6 @@
 // import { ref } from 'vue'
 import ListHeader from '~/components/ListHeader.vue'
 import Drawer from '~/components/Drawer.vue'
-import IconSelect from '~/components/IconSelect.vue'
 // eslint-disable-next-line no-unused-vars
 import {
     getCategoryList,
@@ -120,24 +72,15 @@ const { loading, tableData, getData, handleDelete, handleStatusChange } = initTa
     delete: deleteCategory,
     updateStatus: updateCategoryStatus
 })
-const { drawerRef, formRef, form, rules, drawerTitle, handleAdd, handleEdit, handleSubmit } =
-    initForm({
-        getData,
-        form: {
-            rule_id: 0,
-            menu: 0,
-            name: '',
-            condition: '',
-            method: '',
-            status: 1,
-            order: 20,
-            icon: '',
-            frontpath: ''
-        },
-        create: updateCategory,
-        update: createCategory,
-        loading
-    })
+const { drawerRef, formRef, form, drawerTitle, handleAdd, handleEdit, handleSubmit } = initForm({
+    getData,
+    form: {
+        name: ''
+    },
+    create: updateCategory,
+    update: createCategory,
+    loading
+})
 // 添加子节点
 const addChild = id => {
     handleAdd()
