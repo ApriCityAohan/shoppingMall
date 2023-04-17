69535352c125e902c7b45b35d8816f5d238cb1ea
ApriCityAohan <1448366829@qq.com>
2023-04-17 21:11:27 +0800

    feat：新建用户等级api

---
+++ src/api/level.js
@@ -0,0 +1,16 @@
+import axios from '~/axios'
+export function getUserLevelList(page) {
+    return axios.get(`/admin/user_level/${page}`)
+}
+export function createUserLevel(data) {
+    return axios.post(`/admin/user_level`, data)
+}
+export function updateUserLevel(id, data) {
+    return axios.post(`/admin/user_level/${id}`, data)
+}
+export function updateUserLevelStatus(id, status) {
+    return axios.post(`/admin/user_level/${id}/update_status`, { status })
+}
+export function deleteUserLevel(id) {
+    return axios.post(`/admin/user_level/${id}/delete`)
+}
