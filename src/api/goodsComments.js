5594ef33c96f2690051ffb07eb9639485571331b
ApriCityAohan <1448366829@qq.com>
2023-04-19 21:49:54 +0800

    feat：新增商品回复api

---
+++ src/api/goodsComments.js
@@ -0,0 +1,15 @@
+import axios from '~/axios'
+import { queryParse } from '~/utils/util'
+
+export function getGoodsCommentList(page, query = {}) {
+    const r = queryParse(query)
+    return axios.get(`/admin/goods_comment/${page}${r}`)
+}
+
+export function updateGoodsCommentStatus(id, status) {
+    return axios.post(`/admin/goods_comment/${id}/update_status`, { status })
+}
+
+export function reviewGoodsComment(id, data) {
+    return axios.post(`/admin/goods_comment/review/${id}`, data)
+}
