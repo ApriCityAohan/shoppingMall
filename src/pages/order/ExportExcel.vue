40b6470142dfdf4d74cc121a353d9d1f65328acc
ApriCityAohan <1448366829@qq.com>
2023-04-21 22:14:51 +0800

    feat:实现了文件导出下载

--- src/pages/order/ExportExcel.vue
+++ src/pages/order/ExportExcel.vue
@@ -1,6 +1,6 @@
 <template>
     <el-drawer v-model="drawerViable" title="导出订单" size="40%">
-        <el-form :model="form" label-width="80px" :inline="false" size="normal">
+        <el-form :model="form" label-width="80px" :inline="false">
             <el-form-item label="订单类型">
                 <el-select v-model="form.tab" value-key="" placeholder="">
                     <el-option
@@ -31,6 +31,8 @@
 
 <script setup>
 import { ref, reactive } from 'vue'
+import { toast } from '~/utils/util'
+import { exportOrder } from '~/api/order'
 defineProps({
     tabs: {
         type: Array,
@@ -46,6 +48,36 @@ const drawerViable = ref(false)
 const open = () => (drawerViable.value = true)
 const close = () => (drawerViable.value = false)
 
+const onSubmit = () => {
+    if (!form.tab) return toast('订单类型不能为空', 'error')
+    loading.value = true
+    let starttime = ''
+    let endtime = ''
+    if (form.time && Array.isArray(form.time)) {
+        starttime = form.time[0]
+        endtime = form.time[1]
+    }
+    exportOrder({
+        tab: form.tab,
+        starttime,
+        endtime
+    })
+        .then(data => {
+            const url = window.URL.createObjectURL(new Blob([data]))
+            const link = document.createElement('a')
+            link.style.display = 'none'
+            link.href = url
+            const filename = '订单列表.xlsx'
+            link.setAttribute('download', filename)
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+            close()
+        })
+        .finally(() => {
+            loading.value = false
+        })
+}
 defineExpose({
     open,
     close
