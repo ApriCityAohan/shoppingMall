522f3fae4e1d52f953ab1ace7dfce00c330ffa54
ApriCityAohan <1448366829@qq.com>
2023-04-21 21:25:39 +0800

    feat：更新导出excel 的api

--- src/api/order.js
+++ src/api/order.js
@@ -8,3 +8,7 @@ export function deleteOrder(ids) {
     ids = Array.isArray(ids) ? ids : [ids]
     return axios.post(`/admin/goods/delete_all`, { ids })
 }
+export function exportOrder(query = {}) {
+    const r = queryParse(query)
+    return axios.post(`/admin/order/excelexport${r}`)
+}
