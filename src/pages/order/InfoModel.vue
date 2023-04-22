5d8b20e806f0a4c56595ace333aae79063485d9f
ApriCityAohan <1448366829@qq.com>
2023-04-22 22:11:36 +0800

    feat：多规格tag实现，发货信息、退款信息栏及时间格式化实现

--- src/pages/order/InfoModel.vue
+++ src/pages/order/InfoModel.vue
@@ -1,6 +1,5 @@
 <template>
     <el-drawer v-model="modelViable" title="订单详情" size="50%">
-        <!-- <div class="info-model"></div> -->
         <el-card shadow="never" class="mt-3">
             <template #header>
                 <b class="text-xm">订单信息</b>
@@ -13,10 +12,26 @@
                     {{ info.payment_method }}
                 </el-form-item>
                 <el-form-item label="付款时间">
-                    {{ info.paid_time }}
+                    {{ paidTime }}
                 </el-form-item>
                 <el-form-item label="创建时间">
                     {{ info.create_time }}
+                </el-form-item>
+            </el-form>
+        </el-card>
+        <el-card v-if="info.ship_data" shadow="never" class="mt-3">
+            <template #header>
+                <b class="text-xm">发货信息</b>
+            </template>
+            <el-form label-width="80px">
+                <el-form-item label="物流公司">
+                    {{ info.ship_data.express_company }}
+                </el-form-item>
+                <el-form-item label="运单号">
+                    {{ info.ship_data.express_no }}
+                </el-form-item>
+                <el-form-item label="发货时间">
+                    {{ shipTime }}
                 </el-form-item>
             </el-form>
         </el-card>
@@ -34,8 +49,8 @@
                 ></el-image>
                 <div class="text-sm">
                     <p class="mt-1">{{ item.goods_item?.title ?? '商品已删除' }}</p>
-                    <div v-if="item.skus_type === 1 && item.goods_skus" class="mt-1">
-                        <el-tag type="info" size="small"> </el-tag>
+                    <div v-if="item.sku" class="mt-1">
+                        <el-tag type="info" size="small">{{ item.sku }}</el-tag>
                     </div>
                     <div class="mt-1">
                         <b class="mr-2 text-rose-500">￥{{ item.price }}</b>
@@ -45,7 +60,7 @@
             </div>
             <el-form label-width="80px">
                 <el-form-item label="成交价">
-                    {{ info.total_price }}
+                    <span class="text-rose-500 font-bold text-sm">￥{{ info.total_price }}</span>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -70,17 +85,52 @@
                 </el-form-item>
             </el-form>
         </el-card>
+        <el-card v-if="info.refund_status !== 'pending'" shadow="never" class="mt-3">
+            <template #header>
+                <b class="text-xm">退款信息</b>
+                <el-button text type="primary" disabled class="float-right">{{
+                    refundStatus
+                }}</el-button>
+            </template>
+            <el-form label-width="80px">
+                <el-form-item label="退款理由">
+                    {{ info.extra.refund_reason }}
+                </el-form-item>
+            </el-form>
+        </el-card>
     </el-drawer>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
+import { ref, computed } from 'vue'
+import { useDateFormat } from '@vueuse/core'
+const props = defineProps({
     info: {
         type: Object,
         required: true
     }
+})
+const shipTime = computed(() => {
+    if (props.info.ship_data) {
+        const s = useDateFormat(props.info.ship_data.express_time * 1000, 'YYYY-MM-DD HH:mm:ss')
+        return s.value
+    }
+    return ''
+})
+const refundStatus = computed(() => {
+    const status = {
+        pending: '待处理',
+        applied: '已申请,等待商家处理',
+        processing: '退款中',
+        success: '退款成功',
+        failed: '退款失败'
+    }
+    return props.info.refund_status ? status[props.info.refund_status] : ''
+})
+
+const paidTime = computed(() => {
+    const s = useDateFormat(props.info.paid_time * 1000, 'YYYY-MM-DD HH:mm:ss')
+    return s.value
 })
 
 const modelViable = ref(false)
@@ -94,8 +144,4 @@ defineExpose({
 })
 </script>
 
-<style scoped>
-/* .info-model {
-    position: absolute;
-} */
-</style>
+<style scoped></style>
