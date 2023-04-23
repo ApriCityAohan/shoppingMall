0e25d3151326dc15595d76056091106d0bd68a7f
ApriCityAohan <1448366829@qq.com>
2023-04-23 19:53:42 +0800

    feat：初始化物流查询抽屉

---
+++ src/pages/order/ShipInfoModel.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-drawer v-model="dialogVisible" title="物流信息" size="40%"> </el-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import { getOrderShip } from '~/api/order'
+const dialogVisible = ref(false)
+
+const open = id => {
+    return getOrderShip(id).then(res => {
+        console.log(res)
+        dialogVisible.value = true
+    })
+}
+const close = () => {
+    dialogVisible.value = false
+}
+
+defineExpose({
+    open,
+    close
+})
+</script>
+
+<style scoped></style>
