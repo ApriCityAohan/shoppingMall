46735700602a89c5f1fd03c414e2d01dcaa88e16
ApriCityAohan <1448366829@qq.com>
2023-04-23 21:06:00 +0800

    style:更新样式布局

--- src/pages/order/ShipInfoModel.vue
+++ src/pages/order/ShipInfoModel.vue
@@ -1,16 +1,50 @@
 <template>
-    <el-drawer v-model="dialogVisible" title="物流信息" size="40%"> </el-drawer>
+    <el-drawer v-model="dialogVisible" title="物流信息" size="40%">
+        <el-card shadow="never" class="border-0 mb-3">
+            <div class="flex items-center">
+                <el-image
+                    :src="shipData.logo"
+                    fit="cover"
+                    :lazy="true"
+                    class="rounded border"
+                    style="width: 60px; height: 60px"
+                ></el-image>
+                <div class="ml-2 text-sm">
+                    <p class="mb-2">物流公司：{{ shipData.typename }}</p>
+                    <p>物流单号：{{ shipData.number }}</p>
+                </div>
+            </div>
+        </el-card>
+        <el-card shadow="never" class="border-0 border-t mb-3">
+            <el-timeline class="ml-[-20px]">
+                <el-timeline-item
+                    v-for="(item, index) in shipData.list"
+                    :key="index"
+                    :timestamp="item.time"
+                    placement="top"
+                >
+                    {{ item.status }}
+                </el-timeline-item>
+            </el-timeline>
+        </el-card>
+    </el-drawer>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 // eslint-disable-next-line no-unused-vars
 import { getOrderShip } from '~/api/order'
+import { toast } from '~/utils/util'
 const dialogVisible = ref(false)
+const shipData = ref({})
 
 const open = id => {
     return getOrderShip(id).then(res => {
-        console.log(res)
+        if (res.status !== 0) {
+            return toast(res.msg, 'error')
+        }
+        shipData.value = res.result
+        console.log(shipData.value)
         dialogVisible.value = true
     })
 }
